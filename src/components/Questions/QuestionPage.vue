fa50d74908e75ca3950ca641e6ca0a96e1d42c1d
veliValentine <nikkevanukas1995@gmail.com>
2021-02-25 22:45:31 +0200

    feat: questions from API

--- src/components/Questions/QuestionPage.vue
+++ src/components/Questions/QuestionPage.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="question-page">
     <h1>Questions</h1>
-    <p>{{ questionIndexing }}</p>
-    <question :question="currentQuestion" :selectAnswer="selectAnswer" />
-    <div id="button-container">
-      <button v-show="showPreviousButton" @click="previousQuestion">
-        Previous
-      </button>
-      <button v-show="showNextButton" @click="nextQuestion">Next</button>
-    </div>
-    <div id="finish-container">
-      <router-link to="/results">
+    <template v-if="questions.length > 0">
+      <p>{{ questionIndexing }}</p>
+      <question :question="currentQuestion" :selectAnswer="selectAnswer" />
+      <div id="button-container">
+        <button v-show="showPreviousButton" @click="previousQuestion">
+          Previous
+        </button>
+        <button v-show="showNextButton" @click="nextQuestion">Next</button>
+      </div>
+      <div id="finish-container">
         <button id="finish-trivia" v-on:click="finishTrivia">
           <b>Finish</b>
         </button>
-      </router-link>
-    </div>
+      </div>
+    </template>
+    <template v-else>
+      <p>loading...</p>
+    </template>
   </div>
 </template>
 
@@ -26,108 +29,14 @@ import Question from "./Question.vue";
 export default {
   name: "QuestionPage",
   components: { Question },
+  async created() {
+    const questions = await this.getQuestionsFromAPI();
+    this.questions = questions;
+  },
   data() {
     return {
       index: 0,
-      questions: [
-        {
-          category: "Entertainment: Video Games",
-          type: "multiple",
-          difficulty: "easy",
-          question:
-            "In the game Dark Souls, what is the name of the region you&#039;re in for the majority of the game?",
-          correct_answer: "Lordran",
-          incorrect_answers: ["Drangleic", "Oolacile", "Catarina"],
-        },
-        {
-          category: "Entertainment: Japanese Anime & Manga",
-          type: "boolean",
-          difficulty: "easy",
-          question:
-            "In the &quot;Melancholy of Haruhi Suzumiya&quot; series, the narrator goes by the nickname Kyon.",
-          correct_answer: "True",
-          incorrect_answers: ["False"],
-        },
-        {
-          category: "Entertainment: Video Games",
-          type: "multiple",
-          difficulty: "hard",
-          question:
-            "Which of these weapon classes DO NOT appear in the first Monster Hunter game?",
-          correct_answer: "Bow ",
-          incorrect_answers: ["Hammer", "Heavy Bowgun", "Light Bowgun"],
-        },
-        {
-          category: "Entertainment: Film",
-          type: "multiple",
-          difficulty: "medium",
-          question:
-            "In The Lord of the Rings: The Fellowship of the Ring, which one of the following characters from the book was left out of the film?",
-          correct_answer: "Tom Bombadil",
-          incorrect_answers: ["Strider", "Barliman Butterbur", "Celeborn"],
-        },
-        {
-          category: "Entertainment: Video Games",
-          type: "multiple",
-          difficulty: "easy",
-          question:
-            "Which water-type Pok&eacute;mon starter was introduced in the 4th generation of the series?",
-          correct_answer: "Piplup",
-          incorrect_answers: ["Totodile", "Oshawott", "Mudkip"],
-        },
-        {
-          category: "Geography",
-          type: "multiple",
-          difficulty: "medium",
-          question: "What is the capital of Slovakia?",
-          correct_answer: "Bratislava",
-          incorrect_answers: ["Sofia", "Ljubljana", "Sarajevo"],
-        },
-        {
-          category: "Vehicles",
-          type: "multiple",
-          difficulty: "easy",
-          question:
-            "What are the cylinder-like parts that pump up and down within the engine?",
-          correct_answer: "Pistons",
-          incorrect_answers: ["Leaf Springs", "Radiators", "ABS"],
-        },
-        {
-          category: "Entertainment: Television",
-          type: "multiple",
-          difficulty: "hard",
-          question:
-            "What was the callsign of Commander William Adama in Battlestar Galactica (2004)?",
-          correct_answer: "Husker",
-          incorrect_answers: ["Starbuck", "Apollo", "Crashdown"],
-        },
-        {
-          category: "History",
-          type: "multiple",
-          difficulty: "hard",
-          question:
-            "Which U.S. president was said to have been too honest to lie to his father about chopping down a cherry tree?",
-          correct_answer: "George Washington",
-          incorrect_answers: [
-            "Abraham Lincoln",
-            "Thomas Jefferson",
-            "James Monroe",
-          ],
-        },
-        {
-          category: "History",
-          type: "multiple",
-          difficulty: "easy",
-          question:
-            "Who rode on horseback to warn the Minutemen that the British were coming during the U.S. Revolutionary War?",
-          correct_answer: "Paul Revere",
-          incorrect_answers: [
-            "Thomas Paine",
-            "Henry Longfellow",
-            "Nathan Hale",
-          ],
-        },
-      ],
+      questions: [],
       answers: [],
       totalScore: 0,
     };
@@ -175,13 +84,43 @@ export default {
     },
     finishTrivia() {
       this.$router.push({
-          name: "ResultPage",
-          params: {
-            answers: this.answers,
-            totalScore: this.totalScore,
-          },
-        });
-    }
+        name: "ResultPage",
+        params: {
+          answers: this.answers,
+          totalScore: this.totalScore,
+        },
+      });
+    },
+    getQuestionsFromAPI() {
+      return fetch("https://opentdb.com/api.php?amount=10")
+        .then((response) => response.json())
+        .then(({ results }) => {
+          return results.map((questionObject) => {
+            const {
+              category,
+              correct_answer,
+              difficulty,
+              incorrect_answers,
+              question,
+              type,
+            } = questionObject;
+            return {
+              category: this.parseString(category),
+              correct_answer: this.parseString(correct_answer),
+              difficulty: this.parseString(difficulty),
+              incorrect_answers: incorrect_answers.map((answer) =>
+                this.parseString(answer)
+              ),
+              question: this.parseString(question),
+              type: this.parseString(type),
+            };
+          });
+        })
+        .catch((error) => console.error(error.message));
+    },
+    parseString(string) {
+      return string.replaceAll(/(&quot;)|(&#039;)/g, "'");
+    },
   },
 };
 </script>
